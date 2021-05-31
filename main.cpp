6215ec6bc78aca2b5a3816cf32b88c4daf5c859f
berkayeser <Berkay.Eser@student.uantwerpen.be>
2021-05-31 19:59:21 +0200

    hernaam functie

--- main.cpp
+++ main.cpp
@@ -4,9 +4,62 @@
 #include <time.h>
 using namespace std;
 
+void hernaam(DFA& d){
+    map<string,State*> aa = d.getStates();
+    map<string,State*> newMap;
+
+    map<string,string> replace;
+    int m=0;
+    for(auto& k : aa){
+        string str;
+        /*if(k.first == "DEAD"){
+            str = "{DEAD}";
+            m--;
+        }
+        else{*/
+            str =  to_string(m);
+        //}
+        replace[k.first] = str;
+        k.second->setName(str);
+        newMap[str] = k.second;
+        m++;
+    }
+
+    for(auto& q : aa){
+
+        map<char, vector<string>> newT;
+        for(const auto& p: q.second->getTransitions()){
+            vector<string> newV;
+            for(const auto& f: p.second){
+                newV.push_back(replace[f]);
+            }
+            newT[p.first] = newV;
+        }
+        q.second->setTransitions(newT);
+    }
+
+    d.setStates(newMap);
+
+};
 
 int main() {
-    srand(time(0));
+    DFA dfa("temp.json");
+    map<string,State*> aa = dfa.getStates();
+   //hernaam(dfa);
+
+    //hernaam("1", dfa.getStartState()->getName(), dfa);
+    //for(auto d: dfa)
+    //hernaam("1", dfa.getStartState()->getName(), dfa);
+    //dfa = dfa.minimize();
+    dfa = dfa.minimize();
+    //dfa = dfa.minimize();
+    dfa.print();
+
+    /*srand(time(0));
+
+    Autocorrect a;
+    a.readFile("words.json");
+    //a.run();*/
     //DFA dfa("input-tfa1.json");
     //DFA dfa2("dfatest.json");
 
@@ -37,22 +90,15 @@ int main() {
    //NFA nfa("nfaFile1.json");
    //cout << "nfa accepts: " << nfa.accepts("c");
    //
-   RE re1("DFA", '%');
+   /*RE re1("DFA", '%');
    RE re2("NFA", '%');
 
    DFA dfa1 = re1.toDFA();
    DFA dfa2 = re2.toDFA();
-   DFA dfa3(dfa1,dfa2,true);
+   DFA dfa3(dfa1,dfa2,true);*/
    //dfa3.print();
 
-   Autocorrect a;
 
-   a.readFile("words.json");
-
-
-   //a.mainDFA.minimize().print();
-
-   a.run();
 
    return 0;
 }
